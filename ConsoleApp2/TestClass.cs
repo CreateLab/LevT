24c149c041b02d061a1c086f9aae8f6b9cfeb353
lary <f98f96@gmail.com>
2021-06-05 10:45:18 +0300

    .

--- ConsoleApp2/TestClass.cs
+++ ConsoleApp2/TestClass.cs
@@ -10,10 +10,10 @@ namespace ConsoleApp2
 {
     public class TestClass
     {
-        private static HashSet<string> _hashSet;
-        private static HashSet<string> _littleHash = new HashSet<string>();
+        private  HashSet<string> _hashSet;
+        private  HashSet<string> _littleHash = new HashSet<string>();
 
-        private static Dictionary<string, Dictionary<int, List<string>>> _dictionary =
+        private  Dictionary<string, Dictionary<int, List<string>>> _dictionary =
             new Dictionary<string, Dictionary<int, List<string>>>();
 
         [GlobalSetup]
@@ -22,22 +22,9 @@ namespace ConsoleApp2
             Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
             _hashSet = new HashSet<string>(File.ReadAllLines("Rus.txt", Encoding.GetEncoding(1251)));
             ComputateBaseTrigrams();
-
-            var testClass = new TestClass();
-
-            //если
-            //ладно
-            //привет
-            // конечно
-            // господин
-            /*var enumerable = testClass.Run("добычч", _hashSet, _littleHash, _dictionary);
-            foreach (var s in enumerable)
-            {
-                Console.WriteLine(s);
-            }*/
         }
 
-        private static void ComputateBaseTrigrams()
+        private  void ComputateBaseTrigrams()
         {
             foreach (var word in _hashSet)
             {
@@ -74,10 +61,13 @@ namespace ConsoleApp2
         }
 
         [Params( "добычч", "прявет", "девхшка", "госппдин")]
-        public string paramWord;
+        public string ParamWord { get; set; }
 
+        
+        public int Test1() => 1;
         [Benchmark]
-        public List<string> Test() => Run(paramWord);
+        public List<string> Test() => Run(ParamWord);
+       
         public  List<string> Run(string word) =>
         
             Run(word, _hashSet, _littleHash, _dictionary).ToList();
